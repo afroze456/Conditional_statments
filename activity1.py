8c9fd2db574779e136b1b0ba057ed252fe7a4b6a
afroze456 <afreenafroze1@gmail.com>
2026-04-06 18:28:50 -0700

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,12 @@
+num=3
+if num > 0:
+    print("this number is possitive")
+
+
+num = -1
+if num < 0:
+    print("this number is negitive")
+
+var= int(input("enter any number"))
+if var>0:
+    print("this numebr is posstive")
