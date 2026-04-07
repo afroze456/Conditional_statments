b1a02d4c4c241d86981e43dc785f6468d1165573
afroze456 <afreenafroze1@gmail.com>
2026-04-07 02:25:22 -0700

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,12 @@
+score=float(input("give me the your score "))
+if score>90:
+    print("you got a A")
+
+elif score>80:
+    print("you got a B")
+
+elif score>70:
+    print("you got a C")
+
+else:
+    print("you failed")
